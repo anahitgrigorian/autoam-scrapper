40cf600ea041564e508f03743677460b7417fc05
anahitgrigorian <anahitgrigoryan140@gmail.com>
2025-04-07 15:35:29 +0000

    chore: add new listing handler function

--- src/process-new-listings/app.py
+++ src/process-new-listings/app.py
@@ -3,6 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 import boto3
 import json
+import psycopg2
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
@@ -14,12 +15,15 @@ def lambda_handler(event, context):
 
         if not ip_address:
             raise Exception("AUTOAM_IP_ADDRESS environment variable is not set.")
-        
-        return {"statusCode": 200, "body": json.dumps({"msg": "NOT_IMPLEMENTED"})}
 
-        for record in event['Records']:
-            listing_url = json.loads(record['body'])
-            get_data_from_listing(listing_url, ip_address)
+        # Hardcoded website URL (using the IP address)
+        url = "https://{}/lang/en".format(ip_address)
+
+        # Call the function to get urls from the page
+        page_urls = get_new_urls_from_page(url, ip_address)
+
+        # Put the URLs into an SQS queue
+        put_urls_to_sqs(page_urls)
                 
         # Return the result
         return {"statusCode": 200, "body": json.dumps({"page_urls": page_urls})}
@@ -27,49 +31,123 @@ def lambda_handler(event, context):
         # Handle exceptions and return an error response
         return {"statusCode": 500, "body": json.dumps({"error": str(e)})}
 
-def get_data_from_listing(listing_url, ip_address):
-    # Hardcoded website URL (using the IP address)
-    url = "https://{}/lang/en".format(ip_address)
+def get_new_urls_from_page(url, ip_address):
+    while True:
+        page_number = 1
+        page_urls = []
+        # Step 1: Send an HTTP GET request to the URL
+        response = requests.get(url, verify=False)
 
-    # Step 1: Send an HTTP GET request to the URL
-    response = requests.get(url, verify=False)
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # Extract the cookies and CSRF token from the response
+            autoam_session_cookie = response.cookies.get("autoam_session")
+            xsrf_token_cookie = response.cookies.get("XSRF-TOKEN")
+            soup = BeautifulSoup(response.text, 'html.parser')
+            csrf_token = soup.find('meta', {'name': 'csrf-token'})['content']
 
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        # Extract the cookies and CSRF token from the response
-        autoam_session_cookie = response.cookies.get("autoam_session")
-        xsrf_token_cookie = response.cookies.get("XSRF-TOKEN")
-        soup = BeautifulSoup(response.text, 'html.parser')
-        csrf_token = soup.find('meta', {'name': 'csrf-token'})['content']
+            # Step 2: Make a POST request to the search endpoint using the obtained CSRF token and cookies
+            post_url = "https://{}/search".format(ip_address)  # Update URL for the POST request
+            post_headers = {
+                'Host': 'auto.am',  # Set the Host header
+                'Accept': '*/*',
+                'Accept-Language': 'en-US,en;q=0.5',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                'X-CSRF-Token': csrf_token,
+                'X-Requested-With': 'XMLHttpRequest',
+                'Origin': 'https://auto.am',
+                'Connection': 'keep-alive',
+                'Referer': url,
+                'Cookie': f'XSRF-TOKEN={xsrf_token_cookie}; autoam_session={autoam_session_cookie}'
+            }
 
-        # Step 2: Make a GET request to the listing endpoint using the obtained CSRF token and cookies
-        listing_page = "https://{}{}".format(ip_address, listing_url)  # Update URL for the GET request
-        listing_headers = {
-            'Host': 'auto.am',  # Set the Host header
-            'Accept': '*/*',
-            'Accept-Language': 'en-US,en;q=0.5',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            'X-CSRF-Token': csrf_token,
-            'X-Requested-With': 'XMLHttpRequest',
-            'Origin': 'https://auto.am',
-            'Connection': 'keep-alive',
-            'Referer': url,
-            'Cookie': f'XSRF-TOKEN={xsrf_token_cookie}; autoam_session={autoam_session_cookie}'
-        }
+            post_data = {
+                'search': json.dumps({
+                    "category": "1",
+                    "page": page_number,
+                    "sort": "latest",
+                    "layout": "list",
+                    "user": {"dealer": "0", "official": "0", "id": ""},
+                    "year": {"gt": "1911", "lt": "2025"},
+                    "usdprice": {"gt": "0", "lt": "100000000"},
+                    "custcleared": "1",
+                    "mileage": {"gt": "10", "lt": "10000000"}
+                })
+            }
 
-        # Step 3: Send an HTTP GET request to the listing endpoint with CSRF token, cookies, and data
-        listing_response = requests.get(listing_url, headers=listing_headers, verify=False)
+            # Step 3: Send an HTTP POST request to the search endpoint with CSRF token, cookies, and data
+            post_response = requests.post(post_url, headers=post_headers, data=post_data, verify=False)
 
-        # Check if the POST request was successful (status code 200)
-        if listing_response.status_code == 200:
-            # Extract URLs from the search results
-            soup = BeautifulSoup(listing_response.text, 'html.parser')
-            cars = soup.find_all(class_="card")
-            page_urls = [car.select(".card-image a")[0].get("href") for car in cars]
-
-            return page_urls
+            # Check if the POST request was successful (status code 200)
+            if post_response.status_code == 200:
+                # Extract URLs from the search results
+                soup = BeautifulSoup(post_response.text, 'html.parser')
+                cars = soup.find_all(class_="card")
+                page_new_urls = [car.select(".card-image a")[0].get("href") for car in cars]
+                if check_has_matching(page_new_urls):
+                    page_urls.append(page_new_urls)
+                    return page_urls
+                page_number+=1
+            else:
+                raise Exception("Failed to make the POST request to the search endpoint. Status code: {}".format(post_response.status_code))
         else:
-            raise Exception("Failed to make the POST request to the search endpoint. Status code: {}".format(post_response.status_code))
-    else:
-        raise Exception("Failed to retrieve cookies. Status code: {}".format(response.status_code))
+            raise Exception("Failed to retrieve cookies. Status code: {}".format(response.status_code))
+
+
+def check_has_matching(page_urls):
+    # Get PostgreSQL credentials.
+    smclient = boto3.client('secretsmanager')
+    master_credential = json.loads(smclient.get_secret_value(SecretId=os.environ.get("RDS_SECRET_ARN"))['SecretString'])
+
+    # Connect to the PostgreSQL database
+    conn = psycopg2.connect(
+        host=os.environ.get("RDS_ENDPOINT"),
+        port=os.environ.get("RDS_PORT"),
+        user=master_credential['username'],
+        password=master_credential['password'],
+        database=os.environ.get('RDS_DATABASE_NAME')
+    )
+
+    has_matching = False
+    try:
+        # Create a cursor object to execute SQL queries
+        with conn.cursor() as cursor:
+            for page_url in page_urls:
+                listing_id = page_url.split("/")[2]
+                
+                # Execute a query to check if the listing_id exists in the database
+                cursor.execute("SELECT COUNT(*) FROM cars_raw_data WHERE listing_id = %s", (listing_id,))
+                count = cursor.fetchone()[0]
+
+                if count > 0:
+                    has_matching = True
+                    break
+
+    except Exception as e:
+        print(f"Error: {e}")
+        has_matching = False
+
+    finally:
+        # Close the database connection
+        conn.close()
+
+    return has_matching
+
+def put_urls_to_sqs(urls):
+    sqs_queue_url = os.environ.get("SQS_QUEUE_URL")
+
+    if not sqs_queue_url:
+        raise Exception("SQS_QUEUE_URL environment variable is not set.")
+
+    # Initialize SQS client
+    sqs = boto3.client('sqs')
+
+    # Put each URL to the SQS queue
+    for url in urls:
+        # Send the message to SQS
+        sqs.send_message(
+            QueueUrl=sqs_queue_url,
+            MessageBody=url
+        )
+        
